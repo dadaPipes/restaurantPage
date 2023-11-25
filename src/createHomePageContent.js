09f86bf2dd7ca2fd6a02c6bb473af30fe464f81e
dadaPipes <markoliverstockflethstaf@gmail.com>
2023-11-25 21:30:13 +0100

    style[home]: Add final style, minus footer
    Having serious trouble with the location div.
    The home div is the second cell in the body grid,
    but the location div is outside the home grid.
    
    Temporary fix: Add margin to footer element, to push it down
    from the location div.

--- src/createHomePageContent.js
+++ src/createHomePageContent.js
@@ -1,4 +1,8 @@
 import bird from './images/bird.jpg';
+import restaurantFront from './images/restaurant-front.jpg';
+import restaurant1 from './images/restaurant1.jpg';
+import restaurant2 from './images/restaurant2.jpg';
+import restaurant3 from './images/restaurant3.jpg';
 
 function createHeroSection() {
   const hero = document.createElement('section');
@@ -6,13 +10,13 @@ function createHeroSection() {
 
   const image = document.createElement('img');
   image.classList.add('hero__image');
-  image.src = bird;
+  image.src = restaurantFront;
   image.alt = ''; /* TODO */
   hero.appendChild(image);
 
   const text = document.createElement('p');
   text.classList.add('hero__text');
-  text.innerHTML = 'COZY FOOD <br> COOL VIBES';
+  text.innerHTML = 'COZY FOOD <br> WARM VIBES';
   hero.appendChild(text);
 
   return hero;
@@ -26,19 +30,14 @@ function createIntroductionSection() {
   content.classList.add('introduction-content');
   introduction.appendChild(content);
 
-  const headLine = document.createElement('h3');
-  headLine.classList.add('introduction__headLine');
-  headLine.textContent = 'Restaurant Hygge Haven';
-  content.appendChild(headLine);
-
   const description = document.createElement('p');
   description.classList.add('introduction__description');
-  description.textContent = 'Welcome to "Hygge Haven," a charming Danish restaurant nestled in the heart of the city. Our restaurant embodies the Danish concept of \'hygge\', offering a warm, cozy atmosphere where you can enjoy simple pleasures and create lasting memories.\n\nAt Hygge Haven, we take pride in serving authentic Danish cuisine crafted with care and the finest locally-sourced ingredients. Our menu is a tribute to Denmark\'s rich culinary heritage, featuring timeless dishes like Smørrebrød, Frikadeller, and Rød Pølse.\n\nOur chefs, trained in the heart of Copenhagen, bring a modern flair to these traditional recipes, creating dishes that are both familiar and enticing. Whether you\'re in the mood for a hearty Danish breakfast, a light lunch, or an indulgent dinner, our menu offers something to satisfy every palate.\n\nYet, Hygge Haven is more than just a restaurant; it\'s a haven where you can unwind, relax, and savor the moment. Our interior, inspired by Danish design principles, showcases warm wood tones, soft lighting, and comfortable seating, creating an ambiance that feels like a home away from home.\n\nSo, come and join us at Hygge Haven, and immerse yourself in the warmth, comfort, and delectable flavors of Denmark right here in the city. We eagerly await the opportunity to welcome you.';
+  description.innerHTML = 'Hygge Haven is a charming Danish restaurant nestled in the heart of the city. Our restaurant embodies the Danish concept of \'hygge\', offering a warm, cozy atmosphere where you can enjoy simple pleasures and create lasting memories.<br><br>At Hygge Haven, we take pride in serving authentic Danish cuisine crafted with care and the finest locally-sourced ingredients. Our menu is a tribute to Denmark\'s rich culinary heritage, featuring timeless dishes like Smørrebrød, Frikadeller, and Rød Pølse.<br><br>Our chefs, trained in the heart of Copenhagen, bring a modern flair to these traditional recipes, creating dishes that are both familiar and enticing. Whether you\'re in the mood for a hearty Danish breakfast, a light lunch, or an indulgent dinner, our menu offers something to satisfy every palate.<br><br>Yet, Hygge Haven is more than just a restaurant; it\'s a haven where you can unwind, relax, and savor the moment. Our interior, inspired by Danish design principles, showcases warm wood tones, soft lighting, and comfortable seating, creating an ambiance that feels like a home away from home.<br><br>So, come and join us at Hygge Haven, and immerse yourself in the warmth, comfort, and delectable flavors of Denmark right here in the city. We eagerly await the opportunity to welcome you.';
   content.appendChild(description);
 
   const image = document.createElement('img');
   image.classList.add('introduction__image');
-  image.src = bird;
+  image.src = restaurant1;
   image.alt = ''; /* TODO */
   introduction.appendChild(image);
 
@@ -51,18 +50,8 @@ function createOpeningHoursSection() {
 
   const image = document.createElement('img');
   image.classList.add('opening-hours__image');
-  image.src = bird;
+  image.src = restaurant2;
   image.alt = ''; /* TODO */
-
-  const openingHoursData = {
-    Monday: '8:00 AM - 6:00 PM',
-    Tuesday: '8:00 AM - 6:00 PM',
-    Wednesday: '8:00 AM - 6:00 PM',
-    Thursday: '8:00 AM - 8:00 PM',
-    Friday: '8:00 AM - 8:00 PM',
-    Saturday: '10:00 AM - 4:00 PM',
-    Sunday: 'Closed',
-  };
 
   const container = document.createElement('div');
   container.classList.add('opening-hours__container');
@@ -74,12 +63,37 @@ function createOpeningHoursSection() {
   const list = document.createElement('ul');
   list.classList.add('opening-hours__list');
 
-  Object.entries(openingHoursData).forEach(([day, hours]) => {
+  const openingHoursData = {
+    Monday: '8:00 AM - 6:00 PM',
+    Tuesday: '8:00 AM - 6:00 PM',
+    Wednesday: '8:00 AM - 6:00 PM',
+    Thursday: '8:00 AM - 8:00 PM',
+    Friday: '8:00 AM - 8:00 PM',
+    Saturday: '10:00 AM - 4:00 PM',
+    Sunday: 'Closed',
+  };
+
+  const days = Object.keys(openingHoursData);
+  const hours = Object.values(openingHoursData);
+
+  days.forEach((day, index) => {
     const listItem = document.createElement('li');
     listItem.classList.add('opening-hours__list-item');
-    listItem.textContent = `${day}: ${hours}`;
+   
+    const dayElement = document.createElement('span');
+    dayElement.classList.add('opening-hours__day');
+    dayElement.textContent = day;
+   
+    const hoursElement = document.createElement('span');
+    hoursElement.classList.add('opening-hours__hours');
+    hoursElement.textContent = hours[index];
+   
+    listItem.appendChild(dayElement);
+    listItem.appendChild(hoursElement);
+   
     list.appendChild(listItem);
-  });
+   });
+   
 
   const footer = document.createElement('p');
   footer.classList.add('opening-hours__footer');
@@ -98,14 +112,23 @@ function createLocationSection() {
   const location = document.createElement('section');
   location.classList.add('location');
 
+  const container = document.createElement('div');
+  container.classList.add('location__container');
+  location.appendChild(container);
+
+  const header = document.createElement('h2');
+  header.classList = 'location__header';
+  header.textContent = 'Location';
+  container.appendChild(header);
+
   const address = document.createElement('p');
   address.classList.add('location__address');
-  address.innerHTML = 'Nygade 42 <br> 1202 <br> København K <br> Denmark';
-  location.appendChild(address);
+  address.innerHTML = 'Nyhavn 42 <br> 1202 <br> København K <br> Denmark';
+  container.appendChild(address);
 
   const image = document.createElement('img');
   image.classList.add('location__image');
-  image.src = bird;
+  image.src = restaurant3;
   image.alt = ''; /* TODO */
   location.appendChild(image);
 
@@ -122,8 +145,8 @@ export function createHomePageContent() {
   const introduction = createIntroductionSection();
   home.appendChild(introduction);
 
-  const hours = createOpeningHoursSection();
-  home.appendChild(hours);
+  const openingHours = createOpeningHoursSection();
+  home.appendChild(openingHours);
 
   const location = createLocationSection();
   home.appendChild(location);
